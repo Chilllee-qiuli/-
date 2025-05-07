c8fcf206dfdc5352788494156d38c2ea7e6abc83
Chilllee-qiuli <chilllee2024@163.com>
2025-05-07 16:57:43 +0800

    毕业院校

--- java-server/src/main/java/cn/edu/sdu/java/server/repositorys/StudentRepository.java
+++ java-server/src/main/java/cn/edu/sdu/java/server/repositorys/StudentRepository.java
@@ -22,11 +22,12 @@ public interface StudentRepository extends JpaRepository<Student,Integer> {
     Optional<Student> findByPersonNum(String num);
     List<Student> findByPersonName(String name);
 
-    @Query(value = "from Student where ?1='' or person.num like %?1% or person.name like %?1% ")
+    @Query(value = "from Student where ?1='' or person.num like %?1% or person.name like %?1% or graduateSchool like %?1%")
     List<Student> findStudentListByNumName(String numName);
 
 
     @Query(value = "from Student where ?1='' or person.num like %?1% or person.name like %?1% ",
             countQuery = "SELECT count(personId) from Student where ?1='' or person.num like %?1% or person.name like %?1% ")
     Page<Student> findStudentPageByNumName(String numName,  Pageable pageable);
+
 }
